234193418787143a22836b1c39afeffa49814adb
mondaugen <nicholas.esterer@gmail.com>
2017-01-14 16:22:38 -0500

    Initial commit
    DDM parameter estimation in noise error evaluation completed but requires more
    testing and better plotting.

---
+++ ddm.py
@@ -0,0 +1,102 @@
+# The distribution derivative method and windows it requires
+
+import numpy as np
+
+def w_dw_sum_cos(M,a='hanning'):
+    """
+    Construct sum of cosine windows, based on type or coefficients specified in
+    a.
+
+    M:
+        Integer. Length of window.
+    a:
+        str or iterable. If string, can be:
+            'hann'
+            'hanning'
+            'c1-blackman-4'
+        Otherwise can specify the coefficients of a window.
+
+    Returns:
+    w,dw
+    w:
+        The window.
+    dw:
+        The derivative of window
+    """
+    
+    if (type(a)==type(str())):
+        if (a=='hann') or (a=='hanning'):
+            c=np.r_[0.5,0.5]
+        elif (a=='c1-blackman-4'):
+            c=np.r_[0.358735,0.488305,0.141265,0.011695]
+        elif (a=='c1-nuttall-3'):
+            c=np.r_[0.40897,0.5,0.09103]
+        elif (a=='c1-nuttall-4'):
+            c=np.r_[0.355768,0.487396,0.144232,0.012604]
+    else:
+        try:
+            _it=iter(a)
+        except TypeError:
+            print a, 'is not iterable'
+        c=a
+    m=np.arange(M)
+    w_=((c*np.power(-1,np.arange(len(c))))[:,np.newaxis]
+            *np.cos(np.pi*2./M*np.outer(np.arange(len(c)),m)))
+    w=np.sum(w_,0)
+    dw_=((2.*np.pi/M*c[1:]*np.arange(1,len(c))
+            *np.power(-1,1+np.arange(1,len(c))))[:,np.newaxis]
+        *np.sin(np.pi*2./M*np.outer(np.arange(1,len(c)),m)))
+    dw=np.sum(dw_,0)
+    return (w,dw)
+
+def ddm_p2_1_3(x,w,dw,kma0):
+    """
+    Compute parameters of 2nd order polynomial using 2 bins surrounding the
+    maximum of the STFT at the centre of signal x.
+
+    x:  
+        the signal to analyse, must have length of at least N_w where N_w is
+        the length of the window.
+    w:  
+        the analysis window.
+    dw: 
+        the derivative of the analysis window.
+    kma0:
+        the index of the maximum. From here the atoms are chosen as the one
+        corresponding to this index and the two adjacent ones.
+
+    Returns 
+
+    a:
+        a vector containing the estimated parameters.
+    """
+    N_w=len(w)
+    nx0=np.arange(N_w)
+    x0=x[nx0]
+    Xp1w=np.fft.fft(x0*w)
+    Xp2w=np.fft.fft(2.*nx0*x0*w)
+    Xdw_=np.fft.fft(x0*dw)
+    Xdw=Xp1w*(-2.*np.pi*1j*nx0/N_w)+Xdw_
+    result=[]
+    A=np.c_[
+            np.r_[
+                Xp1w[(kma0)-1:(kma0+2)],
+                ],
+            np.r_[
+                Xp2w[(kma0)-1:(kma0+2)],
+                ]
+            ]
+    c=np.c_[
+            np.r_[
+                Xdw[(kma0)-1:(kma0+2)],
+                ]
+            ]
+    try:
+        a=np.linalg.lstsq(A,-c)[0]
+        gam=np.exp(a[0]*nx0+a[1]*nx0**2.)
+        a0=(np.log(np.inner(x0,np.conj(gam)))
+            -np.log(np.inner(gam,np.conj(gam))))
+        result.append(np.vstack((a0,a)))
+    except ValueError:
+        return None
+    return result
---
+++ ddm_snr_win_comp.py
@@ -0,0 +1,115 @@
+# Compare the accuracy of the DDM using different atoms (windows) and in various
+# SNRs.
+
+import ddm
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Sample rate (Hz)
+F_s=16000
+# Window length (ms)
+W_l_ms=32
+# Window length (samples)
+W_l=F_s*(W_l_ms/1000.)
+# Time indices
+t=np.arange(W_l)/float(F_s)
+
+# Parameter ranges
+# Amplitude part
+a0_r=[0,1.]
+a1_r=[-100.,100.]
+a2_r=[-1.e4,1.e3]
+a3_r=[-1.e5,1.e5]
+# Phase part
+ph0_r=[-np.pi,np.pi]
+ph1_r=[0.,np.pi*F_s]
+ph2_r=[-1.e4,1.e4]
+ph3_r=[-1.e6,1.e6]
+
+# Number of evaluations to perform
+N_eval=1000
+
+# Analysis window, and derivative
+w,dw=ddm.w_dw_sum_cos(W_l,'hann')
+
+# SNRs to test
+snrs=np.arange(-2,11)*(-10.)
+
+# Amplitude part
+a0_errs=[]
+a1_errs=[]
+a2_errs=[]
+a3_errs=[]
+# Phase part
+ph0_errs=[]
+ph1_errs=[]
+ph2_errs=[]
+ph3_errs=[]
+
+for snr in snrs:
+    
+    # Noise gain corresponding to SNR
+    g_no=np.sqrt(10.**(snr/20.))
+    
+    # Amplitude part
+    a0_err=0.
+    a1_err=0.
+    a2_err=0.
+    a3_err=0.
+    # Phase part
+    ph0_err=0.
+    ph1_err=0.
+    ph2_err=0.
+    ph3_err=0.
+
+    n = 0
+    
+    # Order 2 model
+    while (n < N_eval):
+        # Draw random parameters
+        a0=np.random.uniform(a0_r[0],a0_r[1])
+        a1=np.random.uniform(a1_r[0],a1_r[1])
+        a2=np.random.uniform(a2_r[0],a2_r[1])
+        ph0=np.random.uniform(ph0_r[0],ph0_r[1])
+        ph1=np.random.uniform(ph1_r[0],ph1_r[1])
+        ph2=np.random.uniform(ph2_r[0],ph2_r[1])
+        # Synthesize signal
+        # Real argument
+        arg_re=np.polyval([a2,a1,a0],t)
+        # Imaginary argument
+        arg_im=np.polyval([ph2,ph1,ph0],t)
+        # Analytic signal
+        x=np.exp(arg_re+1j*arg_im)
+        # Compute DFT without noise to find maximum
+        X=np.fft.fft(x*w)
+        kma0=np.argmax(np.abs(X))
+        # Add noise
+        y=x+g_no*np.random.standard_normal(int(W_l))
+        # Estimate parameters
+        alpha=ddm.ddm_p2_1_3(y,w,dw,kma0)
+        if (alpha == None):
+            continue
+        # Record errors
+        a0_=np.real(alpha[0][0])
+        a1_=np.real(alpha[0][1])
+        a2_=np.real(alpha[0][2])
+        ph0_=np.imag(alpha[0][0])
+        ph1_=np.imag(alpha[0][1])
+        ph2_=np.imag(alpha[0][2])
+        a0_err+=np.abs(a0-a0_)
+        a1_err+=np.abs(a1-a1_)
+        a2_err+=np.abs(a2-a2_)
+        ph0_err+=np.abs(ph0-ph0_)
+        ph1_err+=np.abs(ph1-ph1_)
+        ph2_err+=np.abs(ph2-ph2_)
+        n += 1
+
+    a0_errs.append(a0_err/float(N_eval))
+    a1_errs.append(a1_err/float(N_eval))
+    a2_errs.append(a2_err/float(N_eval))
+    ph0_errs.append(ph0_err/float(N_eval))
+    ph1_errs.append(ph1_err/float(N_eval))
+    ph2_errs.append(ph2_err/float(N_eval))
+
+plt.plot(snrs,a0_errs)
+plt.show()
