ec24bde6ae8e183a57b9f56c00602a5b5d8d1496
mondaugen <nicholas.esterer@gmail.com>
2017-09-01 19:25:05 -0400

    Prepared one page of references

--- poster/sciposter/dafxposter/build_poster_reduced.sh
+++ poster/sciposter/dafxposter/build_poster_reduced.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export TEXINPUTS=.:../../../paper/:
 pdflatex dafx-poster-reduced.tex
-BSTINPUTS=.:../../../paper/: BIBINPUTS=.:../../../paper/: bibtex dafx-poster-reduced.aux
+BSTINPUTS=.:../../../paper/: BIBINPUTS=.:../../../paper/: bibtex ../../../paper/paper.aux
+#dafx-poster-reduced.aux
 pdflatex dafx-poster-reduced.tex
 pdflatex dafx-poster-reduced.tex
+mv dafx-poster-reduced.pdf Esterer_Nicholas_DAFx_2017_Poster_A0.pdf
